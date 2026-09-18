f3284f3ff8e723569c4e7d0460e102d85fcfc7e1
g2342051 <akaline111@gmail.com>
2026-09-18 23:19:40 +0900

    サイズの調整

--- src/App.jsx
+++ src/App.jsx
@@ -215,6 +215,7 @@ export default function App() {
             
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">最も好きな（または得意な）ジャンル</label>
+              <div>※無い場合は「なし」を記入してください</div>
               <input 
                 type="text" 
                 placeholder="例: HIPHOP, K-POP, Jazzなど"
